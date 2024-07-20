62a4511310b81009b1d2cf739c83ab4a3463bc4e
Aditi Chatterjee <91958958+Aditi0257e@users.noreply.github.com>
2024-07-21 01:36:06 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+
+    <div className='text-xl font-bold text-center'>
+          hello
+      </div>
+        
+    
+  )
+}
+
+export default App
